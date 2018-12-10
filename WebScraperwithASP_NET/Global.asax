162be8a2215c2aee23830f3f60f81d3cc2c4e2d2
surekhasrinivasan <surekhasrao@gmail.com>
2018-12-10 12:08:01 -0500

    Add project files.

---
+++ WebScraperwithASP_NET/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebScraperwithASP_NET.MvcApplication" Language="C#" %>
---
+++ WebScraperwithASP_NET/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebScraperwithASP_NET.Startup))]
+namespace WebScraperwithASP_NET
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
