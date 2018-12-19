d0c19bc32e2faafbc43a9bfb931e66664688f4bf
surekhasrinivasan <surekhasrao@gmail.com>
2018-12-19 18:51:23 -0500

    Cleaned up code in StocksController.cs

--- WebScraperwithASP_NET/Controllers/StocksController.cs
+++ WebScraperwithASP_NET/Controllers/StocksController.cs
@@ -16,50 +16,14 @@ namespace WebScraperwithASP_NET.Controllers
         private StockDatabaseEntities1 db = new StockDatabaseEntities1();
 
         // GET: Stocks
+        [Authorize]
         public ActionResult Index()
         {
             return View(db.Stocks.ToList());
         }
-
-        // GET: Stocks/Details/5
-        public ActionResult Details(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Stock stock = db.Stocks.Find(id);
-            if (stock == null)
-            {
-                return HttpNotFound();
-            }
-            return View(stock);
-        }
-
-        // GET: Stocks/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: Stocks/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-        //[HttpPost]
-        //[ValidateAntiForgeryToken]
-        //public ActionResult Create([Bind(Include = "Id,Symbol,LastPrice,Change,PercentChange,Currency,MarketTime,MarketCap")] Stock stock)
-        //{
-        //    if (ModelState.IsValid)
-        //    {
-        //        db.Stocks.Add(stock);
-        //        db.SaveChanges();
-        //        return RedirectToAction("Index");
-        //    }
-
-        //    return View(stock);
-        //}
-
+        
         // GET: Stocks/Scrape
+        [Authorize]
         public ActionResult Scrape()
         {
             Scraper newScraper = new Scraper();
@@ -106,70 +70,11 @@ namespace WebScraperwithASP_NET.Controllers
             return RedirectToAction("Index");
         }
 
-        // GET: Stocks/Edit/5
-        public ActionResult Edit(int? id)
+        // GET: Stocks/History
+        [Authorize]
+        public ActionResult History()
         {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Stock stock = db.Stocks.Find(id);
-            if (stock == null)
-            {
-                return HttpNotFound();
-            }
-            return View(stock);
-        }
-
-        // POST: Stocks/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "Id,Symbol,LastPrice,Change,PercentChange,Currency,MarketTime,MarketCap")] Stock stock)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(stock).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            return View(stock);
-        }
-
-        // GET: Stocks/Delete/5
-        public ActionResult Delete(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Stock stock = db.Stocks.Find(id);
-            if (stock == null)
-            {
-                return HttpNotFound();
-            }
-            return View(stock);
-        }
-
-        // POST: Stocks/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
-        {
-            Stock stock = db.Stocks.Find(id);
-            db.Stocks.Remove(stock);
-            db.SaveChanges();
-            return RedirectToAction("Index");
-        }
-
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
-                db.Dispose();
-            }
-            base.Dispose(disposing);
-        }
+            return View(db.Histories.ToList());
+        }        
     }
 }
